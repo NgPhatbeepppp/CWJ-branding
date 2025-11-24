59d43421f4140ab4973be02e1e440c0b7968f0f5
NgPhatbeepppp <nphat9975@gmail.com>
2025-11-24 14:20:27 +0700

    HEADER/FOOTER

---
+++ Cw.Branding.Web/Models/Entities/News.cs
@@ -0,0 +1,24 @@
+﻿namespace Cw.Branding.Web.Models.Entities;
+
+public class News
+{
+    public int Id { get; set; }
+
+    public string TitleVi { get; set; } = null!;
+    public string TitleEn { get; set; } = null!;
+
+    public string? SummaryVi { get; set; }
+    public string? SummaryEn { get; set; }
+
+    public string? ContentVi { get; set; }    // HTML từ editor
+    public string? ContentEn { get; set; }
+
+    public string? SlugVi { get; set; }
+    public string? SlugEn { get; set; }
+
+    public bool IsPublished { get; set; } = false;
+    public DateTime? PublishedAt { get; set; }
+
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    public DateTime? UpdatedAt { get; set; }
+}
