a717d723e2383a554c13fca6831dd33902dd417e
NgPhatbeepppp <nphat9975@gmail.com>
2025-11-27 16:55:56 +0700

    FE:Homepage, banner, In progess About Us

--- Cw.Branding.Web/Controllers/HomeController.cs
+++ Cw.Branding.Web/Controllers/HomeController.cs
@@ -1,15 +1,55 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-
+using Microsoft.EntityFrameworkCore;
+using Cw.Branding.Web.Data;
+using Cw.Branding.Web.Models.ViewModels;
 using Cw.Branding.Web.Models;
-
+using Cw.Branding.Web.Models.Entities;
+using System.Diagnostics;
 
 namespace Cw.Branding.Web.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index()
+        private readonly ILogger<HomeController> _logger;
+        private readonly AppDbContext _context;
+
+        public HomeController(ILogger<HomeController> logger, AppDbContext context)
         {
-            return View();
+            _logger = logger;
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var viewModel = new HomeViewModel();
+
+            try
+            {
+                // 1. Fetch Latest News (Giữ nguyên logic lấy 3 bài mới nhất)
+                viewModel.LatestNews = await _context.News
+                    .Where(n => n.IsPublished && n.PublishedAt <= DateTime.Now)
+                    .OrderByDescending(n => n.IsPublished)
+                    .Take(3)
+                    .ToListAsync();
+
+                // 2. Medical Categories: Đã bỏ fetch DB. 
+                // Ở View sẽ hardcode link dẫn tới trang /medical-solutions hoặc /categories
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Error loading Homepage data");
+            }
+
+            return View(viewModel);
+        }
+
+        
+        public IActionResult Privacy() => View();
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
 }
---
+++ Cw.Branding.Web/Models/ViewModels/HomeViewModel.cs
@@ -0,0 +1,16 @@
+﻿using Cw.Branding.Web.Models.Entities;
+
+namespace Cw.Branding.Web.Models.ViewModels
+{
+    public class HomeViewModel
+    {
+       
+
+       
+        public List<News> LatestNews { get; set; } = new List<News>();
+
+        // SEO Meta
+        public string MetaTitle { get; set; } = "Charles Wembley - Corporate Branding";
+        public string MetaDescription { get; set; } = "Leading Medical & F&B Solutions Provider";
+    }
+}
