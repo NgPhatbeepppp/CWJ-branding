54726f85b2d176645a596ab9e6ac7a26ec2b1c97
NgPhatbeepppp <yphat732@gmail.com>
2026-05-11 13:55:55 +0700

    feat/update: About

--- Cw.Branding.Web/obj/Debug/net8.0/Cw.Branding.Web.AssemblyInfo.cs
+++ Cw.Branding.Web/obj/Debug/net8.0/Cw.Branding.Web.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Cw.Branding.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a1d065f685377c2a7179b9334c6a063037a91150")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8f8c7c3e5edc98b2f9c17a6797efc0e99727be79")]
 [assembly: System.Reflection.AssemblyProductAttribute("Cw.Branding.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Cw.Branding.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
